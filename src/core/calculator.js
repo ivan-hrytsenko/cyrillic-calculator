6d8fd5494a452d83e363962f0b84b9573479e805
ivan-hrytsenko <ivan.hrytsenko123@gmail.com>
2025-06-06 00:43:56 +0300

    implement `calculator.js`

---
+++ src/core/calculator.js
@@ -0,0 +1,37 @@
+const OPERATION_ADD = '+';
+const OPERATION_SUBTRACT = '-';
+const OPERATION_MULTIPLY = '*';
+const OPERATION_DIVIDE = '/';
+
+const calculate = (firstOperand, secondOperand, operation) => {
+  let result;
+  
+  switch (operation) {
+    case OPERATION_ADD:
+      result = firstOperand + secondOperand;
+      break;
+    case OPERATION_SUBTRACT:
+      result = firstOperand - secondOperand;
+      break;
+    case OPERATION_MULTIPLY:
+      result = firstOperand * secondOperand;
+      break;
+    case OPERATION_DIVIDE:
+      if (secondOperand === 0) {
+        return NaN;
+      }
+      result = firstOperand / secondOperand;
+      break;
+    default:
+      return NaN;
+  }
+  return result;
+};
+
+export {
+  calculate,
+  OPERATION_ADD,
+  OPERATION_SUBTRACT,
+  OPERATION_MULTIPLY,
+  OPERATION_DIVIDE
+};
