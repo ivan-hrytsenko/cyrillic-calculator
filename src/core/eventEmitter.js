4d56ff0f0d6b7181d1c74241d6e6ab90c97e85c9
ivan-hrytsenko <ivan.hrytsenko123@gmail.com>
2025-06-24 10:30:09 +0300

    implement `eventEmitter.js`

---
+++ src/core/eventEmitter.js
@@ -0,0 +1,42 @@
+class EventEmitter {
+    constructor() {
+        this.listeners = new Map();
+    }
+
+    on(eventName, callback) {
+        if (!this.listeners.has(eventName)) {
+            this.listeners.set(eventName, new Set());
+        }
+        this.listeners.get(eventName).add(callback);
+        return () => this.off(eventName, callback);
+    }
+
+    off(eventName, callback) {
+        if (this.listeners.has(eventName)) {
+            this.listeners.get(eventName).delete(callback);
+            if (this.listeners.get(eventName).size === 0) {
+                this.listeners.delete(eventName);
+            }
+        }
+    }
+
+    emit(eventName, ...args) {
+        if (this.listeners.has(eventName)) {
+            Array.from(this.listeners.get(eventName)).forEach(callback => {
+                try {
+                    callback(...args);
+                } catch (error) {
+                    console.error(`Error in event listener for "${eventName}":`, error);
+                }
+            });
+        }
+    }
+
+    listenerCount(eventName) {
+        return this.listeners.has(eventName) ? this.listeners.get(eventName).size : 0;
+    }
+
+    clearAllListeners() {
+        this.listeners.clear();
+    }
+}
